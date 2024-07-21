ab22a3cee6a654799d05d830fa4038f096242c8a
naser-kazemi <naserkazemi2002@gmail.com>
2024-07-21 20:13:20 +0300

    Edit Project

---
+++ run_tap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Path to your Python executable
+PYTHON_EXECUTABLE=$(which python)
+
+# Path to your Python script that manages the TUN/TAP device
+SCRIPT_PATH="setup_tap.py"
+
+# Execute the Python script
+sudo $PYTHON_EXECUTABLE $SCRIPT_PATH
