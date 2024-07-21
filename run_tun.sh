8753c58be0763bcbb32d3d6d3d62c8d02870eb5b
naser-kazemi <naserkazemi2002@gmail.com>
2024-07-21 17:06:04 +0300

    Edit Project

--- run_tun.sh
+++ run_tun.sh
@@ -6,12 +6,8 @@ PYTHON_EXECUTABLE=$(which python)
 # Path to your Python script that manages the TUN/TAP device
 SCRIPT_PATH="setup_tun.py"
 
-# Ensure that the Python executable has the necessary capabilities
-# CAP_NET_ADMIN allows the manipulation of network interfaces
-#sudo setcap cap_net_admin=eip $PYTHON_EXECUTABLE
-
-# Verify the capabilities
-#getcap $PYTHON_EXECUTABLE
-
 # Execute the Python script
-$PYTHON_EXECUTABLE $SCRIPT_PATH
+sudo $PYTHON_EXECUTABLE $SCRIPT_PATH
+
+# Add route to the custom table
+sudo ./set_routes.sh
