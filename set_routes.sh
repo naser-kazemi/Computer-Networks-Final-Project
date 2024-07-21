9bf174f7e3b4b3c5c708ec9b1433456d66d7773b
naser-kazemi <naserkazemi2002@gmail.com>
2024-07-21 16:02:04 +0300

    Edit Project

--- set_routes.sh
+++ set_routes.sh
@@ -5,6 +5,7 @@ DOMAIN="neverssl.com"
 
 # Network through which traffic should be routed
 TARGET_NETWORK="172.16.0.0/24"
+NIC="tun0"
 
 # Resolve the domain name to an IP address
 IP_ADDRESS=$(dig +short $DOMAIN | tail -n1)
@@ -20,6 +21,7 @@ echo "Resolved IP for $DOMAIN is $IP_ADDRESS"
 sudo ip route add $IP_ADDRESS dev tun0
 
 # Apply the iptables rule to mark the packets
-sudo iptables -t nat -A POSTROUTING -s $TARGET_NETWORK ! -d $TARGET_NETWORK -j MASQUERADE
+#sudo iptables -t nat -A POSTROUTING -s $TARGET_NETWORK ! -d $TARGET_NETWORK -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o $NIC -j MASQUERADE
 
 echo "Routing set for $DOMAIN to go through $TARGET_NETWORK"
