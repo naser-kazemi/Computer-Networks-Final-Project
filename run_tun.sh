cddd4893e7a1aed338611e0ea5f497099473dec5
Naser Kazemi <naserkazemi2002@gmail.com>
2024-08-19 00:10:17 +0300

    Edit Project

---
+++ run_tun.sh
@@ -0,0 +1,2 @@
+sudo ip addr add 172.16.0.2/24 dev tun0
+sudo iptables -t nat -A POSTROUTING -s 172.16.0.2/24 ! -d 172.16.0.2/24 -j MASQUERADE
--- tun.py
+++ tun.py
@@ -13,17 +13,18 @@ def create_tun_interface(interface_name='tun0', subnet='172.16.0.0/24'):
         # Bring the interface up
         subprocess.run(['sudo', 'ip', 'link', 'set', 'dev', interface_name, 'up'], check=True)
 
-        subprocess.run(['sudo', 'ip', 'addr', 'add', subnet, 'dev', interface_name], check=True)
+        # sudo ip addr add subnet dev interface_name
+        # subprocess.run(['sudo', 'ip', 'addr', 'add', subnet, 'dev', interface_name], check=True)
 
         # Set the IP address of the interface
         # subprocess.run(['sudo', 'ip', 'addr', 'add', '172.16.0.0', 'dev', interface_name], check=True)
 
-        time.sleep(2)
+        # time.sleep(2)
 
         # iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
-        subprocess.run(
-            ['sudo', 'iptables', '-t', 'nat', '-A', 'POSTROUTING', '-s', subnet, '! -d', subnet, '-j', 'MASQUERADE'],
-            check=True)
+        # subprocess.run(
+        #     ['sudo', 'iptables', '-t', 'nat', '-A', 'POSTROUTING', '-s', subnet, '! -d', subnet, '-j', 'MASQUERADE'],
+        #     check=True)
 
         print(f"TUN interface {interface_name} created successfully.")
 
