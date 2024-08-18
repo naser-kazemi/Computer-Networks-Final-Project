2c680e3f1555f37026d542f5d670fdb36b7d17d3
Naser Kazemi <naserkazemi2002@gmail.com>
2024-08-19 00:01:40 +0300

    Edit Project

--- setup_tun.py
+++ setup_tun.py
@@ -7,11 +7,10 @@ import socket
 def main():
     try:
         tun_name = 'tun0'
-        delete_tun_interface(tun_name)
         create_tun_interface(tun_name, subnet='172.16.0.2/24')
         setup_routing_by_ip(nic=tun_name, subnet='172.16.0.2/24')
         setup_routing_by_domain(nic=tun_name, domain='neverssl.com')
-    
+
         parser = PacketParser()
         tun = open_tun_interface(tun_name)
         print(f"TUN interface {tun_name} is opened.")
