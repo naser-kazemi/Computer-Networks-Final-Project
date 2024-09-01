d634426ca6849fc2ac3717ca94e1fc1b6df51105
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-01 23:50:41 +0300

    Edit Project

--- run_tun.sh
+++ run_tun.sh
@@ -4,11 +4,13 @@ SUBNET="172.16.0.2/24"
 # create the tun device
 sudo ip tuntap add dev $NIC mode tun
 
+sudo ip link set dev tun0 up
+
 sudo ip addr flush dev $NIC
 sudo ip addr add $SUBNET dev $NIC
 
 # get the ip address of the neverssl.com server using dig
-IP_ADDRESS=$(dig +short neverssl.com)
+IP_ADDRESS=$(d)
 sudo ip route add $IP_ADDRESS dev $NIC
 
 sudo iptables -t nat -A POSTROUTING -s $SUBNET ! -d $SUBNET -j MASQUERADE
