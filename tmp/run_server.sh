54a42887d914f627a169970ec1d300025a5cb731
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-04 22:41:50 +0300

    Edit Project

--- tmp/run_server.sh
+++ tmp/run_server.sh
@@ -6,7 +6,7 @@ PYTHON_EXECUTABLE=$(which python)
 
 cleanup() {
   echo "Cleaning up..."
-  iptables -t nat -D POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
+  sudo iptables -t nat -D POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
   echo "iptables rule removed"
   exit 0
 }
@@ -14,7 +14,7 @@ cleanup() {
 trap cleanup INT TERM
 
 sudo sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
 sudo PYTHON_EXECUTABLE main.py --mode server --subnet 172.16.0.1/24 --port 12345
