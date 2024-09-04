be78cdf5cfa028b78de83de5ccfe2e917a77680e
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-04 22:24:55 +0300

    Edit Project

--- main_client.py
+++ main_client.py
@@ -7,7 +7,7 @@ def main():
     args = get_args()
 
     tun_name = args.tun_name
-    server_ip = args
+    server_ip = args.server_ip
     port = args.port
 
     server = TunClient(tun_name, server_ip, port, SECRET)
