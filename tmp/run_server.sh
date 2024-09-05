14cc5dd0d17341f2172be7408a36fd847206e787
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-05 17:28:13 +0300

    Edit Project

--- tmp/run_server.sh
+++ tmp/run_server.sh
@@ -1,3 +1,7 @@
+source ../.venv/bin/activate
+
+# Path to your Python executable
+PYTHON_EXECUTABLE=$(which python)
 
 cleanup() {
   echo "Cleaning up..."
@@ -12,6 +16,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
-sudo python3 main.py --mode server --subnet 172.16.0.1/24 --port 12345
+sudo $PYTHON_EXECUTABLE main.py --mode server --subnet 172.16.0.1/24 --port 12345
 
 trap cleanup EXIT
