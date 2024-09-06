a2491b32444439fcd91e234a31b9ef6dcdae0e62
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 09:46:09 +0300

    Edit Project

--- run_server.sh
+++ run_server.sh
@@ -8,6 +8,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
-sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name tun0 --subnet 172.16.0.1/24 --port 8080
+sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name tun0 --subnet 172.16.0.2/24 --port 8080
 
 trap cleanup EXIT
