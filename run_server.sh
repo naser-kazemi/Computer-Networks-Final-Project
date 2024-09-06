b99b5b8edc456c8014fefae794231a4540c7e52d
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 10:15:28 +0300

    Edit Project

--- run_server.sh
+++ run_server.sh
@@ -17,7 +17,7 @@ sudo ip addr add $SUBNET dev $NIC
 
 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s $SUBNET ! -d $SUBNET -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
 sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name tun0 --port 8080
