fdafb8516fb407cb6dc1d1ab8c3ba75b4ec7281c
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 09:44:27 +0300

    Edit Project

--- run_server.sh
+++ run_server.sh
@@ -3,14 +3,6 @@ source .venv/bin/activate
 # Path to your Python executable
 PYTHON_EXECUTABLE=$(which python)
 
-cleanup() {
-  echo "Cleaning up..."
-  sudo iptables -t nat -D POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
-  echo "iptables rule removed"
-  exit 0
-}
-
-trap cleanup INT TERM
 
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
