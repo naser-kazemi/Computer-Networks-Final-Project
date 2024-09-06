234ba918b9b27763a0b840fe7f40bd8b6fc10896
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 09:40:42 +0300

    Edit Project

--- run_server.sh
+++ run_server.sh
@@ -4,16 +4,15 @@ source .venv/bin/activate
 PYTHON_EXECUTABLE=$(which python)
 
 NIC="tun0"
-SUBNET="172.16.0.2/24"
 
 
 trap cleanup INT TERM
 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s $SUBNET ! -d $SUBNET -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
-sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name $NIC --subnet $SUBNET --port 8080
+sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name $NIC --subnet 172.16.0.2/24 --port 8080
 
 
 sudo ip tuntap del dev $NIC mode tun
