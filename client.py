d8197318156f40cb371179f941c9c5c85ea69bd9
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 11:44:51 +0300

    A reconnecting to server.

--- client.py
+++ client.py
@@ -16,6 +16,7 @@ class TunClient(TunBase):
         self.server_port = port
         self.connected = False
         self.reconnect_thread = None
+        self.connection_check_thread = None
 
     def start(self):
         print_colored(
@@ -29,16 +30,30 @@ class TunClient(TunBase):
         self.reconnect_thread = threading.Thread(target=self.reconnect_loop)
         self.reconnect_thread.start()
         
+        self.connection_check_thread = threading.Thread(target=self.check_connection)
+        self.connection_check_thread.start()
+        
         self.run_state.is_running = True
 
         super().start(self.run_state)
 
     def reconnect_loop(self):
-        while True:
+        while self.run_state.is_running:
             if not self.connected:
                 print_colored("Attempting to reconnect...", Color.YELLOW)
                 self.connect_to_server()
             time.sleep(5)
+
+    def check_connection(self):
+        while self.run_state.is_running:
+            try:
+                # Send a small packet to check if the connection is still alive
+                self.sock.sendto(b'', (self.server_host, self.server_port))
+            except socket.error:
+                print_colored("Connection lost", Color.RED)
+                self.connected = False
+                self.run_state.is_running = False
+            time.sleep(1)
 
     def connect_to_server(self):
         try:
