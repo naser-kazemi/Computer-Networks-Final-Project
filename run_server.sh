872f27264691cb20d3c8814a10fcb5bb113dbfe8
Naser Kazemi <naserkazemi2002@gmail.com>
2024-09-06 09:43:40 +0300

    Edit Project

--- run_server.sh
+++ run_server.sh
@@ -3,8 +3,12 @@ source .venv/bin/activate
 # Path to your Python executable
 PYTHON_EXECUTABLE=$(which python)
 
-NIC="tun0"
-
+cleanup() {
+  echo "Cleaning up..."
+  sudo iptables -t nat -D POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
+  echo "iptables rule removed"
+  exit 0
+}
 
 trap cleanup INT TERM
 
@@ -12,9 +16,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 172.16.0.0/24 ! -d 172.16.0.0/24 -j MASQUERADE
 echo "NAT configuration is done"
 
-sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name $NIC --subnet 172.16.0.2/24 --port 8080
+sudo $PYTHON_EXECUTABLE main.py --mode server --tun-name tun0 --subnet 172.16.0.1/24 --port 8080
 
-
-sudo ip tuntap del dev $NIC mode tun
-
-echo "Tun device $NIC deleted"
+trap cleanup EXIT
